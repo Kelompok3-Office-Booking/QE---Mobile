9215d1558be6505a1955c53ca2d0e3876e86922a
AidaParamita <99674479+AidaParamita@users.noreply.github.com>
2022-12-16 12:25:08 +0700

    add search

---
+++ Mobile_Capstone-Project/Scripts/Homepage/Search/TCSearch_Building/Script1670946360948.groovy
@@ -0,0 +1,80 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+// 1
+
+Mobile.startApplication('C:\\Users\\AIDA\\Downloads\\betterspace - update 15-12-22.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/OnBoarding/android.widget.Button (3)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/OnBoarding/android.widget.Button (4)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Login/android.widget.EditText - Email'), 0)
+
+Mobile.setText(findTestObject('Object Repository/Login/android.widget.EditText - Emailexamplegmail.com'), GlobalVariable.globalEmail, 
+    0)
+
+Mobile.tap(findTestObject('Object Repository/Login/android.widget.EditText - Password'), 0)
+
+Mobile.setText(findTestObject('Object Repository/Login/android.widget.EditText - Password (1)'), GlobalVariable.globalPass, 
+    0)
+
+Mobile.hideKeyboard()
+
+Mobile.tap(findTestObject('Object Repository/Search_Location2/android.widget.Button (2)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Search_Location2/android.view.View - Where do you want to work today'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Search_Location2/android.view.View - With whom did you come here Mau kerja dimana hari ini'), 0)
+
+Mobile.closeApplication()
+
+// 2
+
+Mobile.startApplication('C:\\Users\\AIDA\\Downloads\\betterspace - update 15-12-22.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/OnBoarding/android.widget.Button'), 0)
+
+Mobile.tap(findTestObject('Object Repository/OnBoarding/android.widget.Button (1)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/OnBoarding/android.widget.Button (1)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/OnBoarding/android.widget.Button (2)'), 0)
+
+Mobile.delay(2)
+
+Mobile.tap(findTestObject('Object Repository/Login/android.widget.EditText - Email'), 0)
+
+Mobile.setText(findTestObject('Object Repository/Login/android.widget.EditText - Emailexamplegmail.com'), GlobalVariable.globalEmail,
+	0)
+
+Mobile.tap(findTestObject('Object Repository/Login/android.widget.EditText - Password'), 0)
+
+Mobile.setText(findTestObject('Object Repository/Login/android.widget.EditText - Password (1)'), GlobalVariable.globalPass,
+	0)
+
+Mobile.hideKeyboard()
+
+Mobile.tap(findTestObject('Object Repository/Search_Location2/android.widget.Button (2)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Search_Location2/android.view.View - Where do you want to work today'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Search_Location2/android.view.View - With whom did you come here Mau kerja dimana hari ini'), 0)
+
+Mobile.closeApplication()
